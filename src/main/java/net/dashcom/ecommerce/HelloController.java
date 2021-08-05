f75ce41bc9b0db846598ab275ef7491d1ac26dcb
Danny <vonnenaut@users.noreply.github.com>
2021-08-05 16:25:07 +0700

    u/d

--- src/main/java/net/dashcom/ecommerce/HelloController.java
+++ src/main/java/net/dashcom/ecommerce/HelloController.java
@@ -1,6 +1,5 @@
 package net.dashcom.ecommerce;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/net/dashcom/ecommerce/MainController.java
+++ src/main/java/net/dashcom/ecommerce/MainController.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
-    public String home() {
-        return "index.html";
+    public String home(Model model) {
+        String helloworld = "in style with mustache";
+        model.addAttribute("message", helloworld);
+
+        return "index.mustache";
     }
 }
